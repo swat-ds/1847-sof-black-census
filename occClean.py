1acdec4d8f6d5289ea5df033e98d5d49f2451f03
Alice Huang <ahuang2@swarthmore.edu>
2019-07-12 16:39:52 -0400

    add OpenRefine export csv and Python code

---
+++ occClean.py
@@ -0,0 +1,47 @@
+# Clean male occupation compensation data from 1847 census data
+# Alice Huang, 7/12/19
+
+import csv
+
+def main():
+  # init vars
+  list = []
+  empty = '. . . . . . . . . . .'
+
+  with open('OccTable1Data.csv') as f:
+      reader = csv.reader(f, delimiter=',')
+      for row in reader:
+          clear = False
+          occ = row[1]
+          if occ == empty: # check for empty row
+              clear = True
+
+          cleanOcc = ''
+          cleanOcc2 = ''
+
+          if not clear: # only do this if not empty row
+              for i in range(len(occ)): # take out periods
+                  if occ[i].isalpha() or occ[i] == ' ':
+                      cleanOcc += occ[i]
+
+              if cleanOcc[0] != ' ': # only add the first char if not a space (otherwise extra space at beginning)
+                  cleanOcc2 += cleanOcc[0]
+              for i in range(1, len(cleanOcc)): # split each row into approp num of different occupations
+                  if cleanOcc[i].isalpha():
+                      cleanOcc2 += cleanOcc[i]
+                  else:
+                      if cleanOcc[i-1] == ' ': # if previous char was space
+                          if cleanOcc2 != '': # if this new occ is not empty
+                              list.append([cleanOcc2])
+                              cleanOcc2 = ''
+                      else:
+                          cleanOcc2 += ' '
+          else:
+              list.append(['null'])
+
+  with open('CleanOccTable1.csv', 'w') as f:
+      writer = csv.writer(f, delimiter=',')
+      writer.writerows(list)
+
+
+main()
