74af700c7dc8cb6e6207f1dfb3e2472150d61e39
Alice Huang <ahuang2@swarthmore.edu>
2019-07-19 17:01:55 -0400

    add 3rd category occ viz, revise scope of 1st viz

--- OccProto/main.js
+++ OccProto/main.js
@@ -5,7 +5,7 @@ Alice Huang, 7/17/19 */
 var chart = c3.generate({
     bindto: '#chart',
     data: {
-      url: 'testdata.csv',
+      url: 'data/testdata.csv',
       x: 'OCC',
       type: 'bar',
       groups: [
@@ -38,7 +38,7 @@ var chart = c3.generate({
 var chart2 = c3.generate({
     bindto: '#chart2',
     data: {
-      url: 'testdata3.csv',
+      url: 'data/testdata3.csv',
       x: 'OCCUPATION',
       type: 'scatter',
       colors: {
@@ -71,14 +71,16 @@ var chart2 = c3.generate({
     }
 });
 
+// event listeners for chart 2 scatter plot - filter data by sex
+
 let current = '';
 
 d3.select('#female-filter button').on('click', function() {
-  current = 'female'
+  current = 'female' // remember the last-clicked button for both filtering
   setTimeout(function() {
       chart2.load({
-        unload: true,
-        url: 'testdata4.csv',
+        unload: true, // on click, clear the loaded data and load new data
+        url: 'data/testdata4.csv', // load from second csv file
         colors: {
           'FEMALE': 'red'
         }
@@ -91,7 +93,7 @@ d3.select('#male-filter button').on('click', function() {
   setTimeout(function() {
       chart2.load({
         unload: true,
-        url: 'testdata3.csv',
+        url: 'data/testdata3.csv',
         colors: {
           'MALE': '#12cadb'
         }
@@ -103,7 +105,7 @@ d3.select('#both-filter button').on('click', function() {
   if (current == 'female'){
     setTimeout(function() {
       chart2.load({
-        url: 'testdata3.csv',
+        url: 'data/testdata3.csv',
         colors: {
           'MALE': '#12cadb',
         }
@@ -112,7 +114,7 @@ d3.select('#both-filter button').on('click', function() {
   } else {
     setTimeout(function() {
       chart2.load({
-        url: 'testdata4.csv',
+        url: 'data/testdata4.csv',
         colors: {
           'FEMALE': 'red',
         }
@@ -121,35 +123,80 @@ d3.select('#both-filter button').on('click', function() {
   }
 })
 
-// scatter plot for female occ data
-// var chart3 = c3.generate({
-//     bindto: '#chart3',
-//     data: {
-//       url: 'testdata4.csv',
-//       x: 'OCCUPATION2',
-//       type: 'scatter',
-//       colors: {
-//         'FEMALE': 'red'
-//       }
-//     },
-//     axis: {
-//       x: {
-//         type: 'category',
-//         tick: {
-//           rotate: 60,
-//           multiline: false
-//         },
-//         label: 'Occupation'
-//       },
-//       y: {
-//         max: 4500,
-//         label: 'ID'
-//       }
-//     },
-//     size: {
-//       height: 500,
-//     },
-//     point: {
-//       r: 3
-//     }
-// });
+let chart3,
+    currCategory;
+
+drawCategoryChart('data/m_p.csv', 'PUBLIC_SERVICE')
+
+function drawCategoryChart(file, category) {
+  chart3 = c3.generate({
+      bindto: '#chart3',
+      data: {
+        url: file,
+        x: category,
+        type: 'bar',
+        colors: {
+          'MALE': '#12cadb',
+          'FEMALE': 'red'
+        }
+      },
+      transition: {
+        duration: 2000
+      },
+      axis: {
+        x: {
+          type: 'category',
+          tick: {
+            rotate: 60,
+            multiline: false
+          },
+          label: 'Occupation'
+        },
+        y: {
+          label: 'Frequency',
+        }
+      },
+      size: {
+        height: 500,
+        width: window.innerWidth/2
+      },
+  });
+
+}
+
+function reload(file) {
+  setTimeout(function() {
+      chart3.load({
+        unload: true,
+        url: file,
+      });
+  }, 300);
+}
+
+d3.select('#p-filter-f button').on('click', function() {
+  reload('data/f_' + currCategory + '.csv')
+})
+
+d3.select('#p-filter-m button').on('click', function() {
+  reload('data/m_' + currCategory + '.csv')
+})
+
+d3.select('#cat-filter select').on('change', function() {
+  item = d3.select(this).property('value'); // get drop-down selection
+  if(item == 'd') {
+    drawCategoryChart('data/m_d.csv', 'DOMESTIC')
+    currCategory = 'd'
+  } else if (item == 's') {
+    drawCategoryChart('data/m_s.csv', 'SKILLED_TRADES')
+    currCategory = 's'
+  } else if (item == 'u') {
+    drawCategoryChart('data/m_u.csv', 'UNSKILLED_LABOR')
+    currCategory = 'u'
+  } else if (item == 'm') {
+    drawCategoryChart('data/m_m.csv', 'MISCELLANEOUS')
+    currCategory = 'm'
+  } else {
+    drawCategoryChart('data/m_p.csv', 'PUBLIC_SERVICE')
+    currCategory = 'p'
+  }
+})
